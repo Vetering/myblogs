520a00917fd6ce97d523da558467cd5516566414
ingVeter <100694672+Vetering@users.noreply.github.com>
2023-05-25 08:51:47 +0800

    Add files via upload

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-$servername ="127.0.0.1";
+$servername = "127.0.0.1";
 $dbname = "sqlqq7684490";
 $username = "qq7684490";
 $password = "qq7684490";
@@ -32,14 +32,22 @@ if (!$conn->query("DESCRIBE users")) {
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-// 将数据插入到数据库中
-$sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+// 查询数据库中是否已存在该用户名
+$sql_check = "SELECT id FROM users WHERE username='$username'";
+$result = $conn->query($sql_check);
 
-if ($conn->query($sql) === TRUE) {
-  echo "注册成功！";
-  header("Location: login.html");
+if ($result && $result->num_rows > 0) {
+  // 如果存在，则提示用户已存在，直接登录或注册
+  echo "该用户已存在！请直接登录或注册！";
 } else {
-  echo "注册失败！请重试！ " . $sql . "<br>" . $conn->error;
+  // 如果不存在，则将数据插入到数据库中
+  $sql_insert = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+  if ($conn->query($sql_insert) === TRUE) {
+    echo "注册成功！";
+    header("Location: login.html");
+  } else {
+    echo "注册失败！请重试！ " . $sql_insert . "<br>" . $conn->error;
+  }
 }
 
 // 关闭连接
